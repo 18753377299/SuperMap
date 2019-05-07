8798aca7d15983f4a3d6b6dfdaa6088ecf5b7d79
1733856225@qq.com <1733856225@qq.com>
2019-05-07 10:27:25 +0800

    iobjectjava创建面数据，半径可以参考前台OA的半径方式

--- src/createDataSource/OperateDataSet.java
+++ src/createDataSource/OperateDataSet.java
@@ -1,21 +1,27 @@
 package createDataSource;
 
+import com.supermap.data.CursorType;
+import com.supermap.data.DatasetType;
 import com.supermap.data.DatasetVector;
+import com.supermap.data.DatasetVectorInfo;
+import com.supermap.data.Datasets;
 import com.supermap.data.Datasource;
 import com.supermap.data.DatasourceConnectionInfo;
 import com.supermap.data.EngineType;
 import com.supermap.data.GeoCircle;
+import com.supermap.data.GeoRegion;
 import com.supermap.data.Point2D;
+import com.supermap.data.Recordset;
 import com.supermap.data.Workspace;
 	/**
  * @author  作者 E-mail: 
- * @date 创建时间：2019年5月6日 下午6:54:31
+ * @date 创建时间：2019年5月7日 上午9:42:20
  * @version 1.0 
  * @parameter 
  * @since  
  * @return  */
-public class OperateDataSource {
-	/*静态变量*/
+public class OperateDataSet {
+/*静态变量*/
 	
 	private static final  String  iobjectJavaServer = "10.10.68.248:1521/orcl";
 	private static final  String  iobjectJavaDatabase = "riskcontrol";
@@ -23,59 +29,13 @@ public class OperateDataSource {
 	private static final  String  iobjectJavaPassword = "riskcontrol";
 	private static final  String  riskMap_address  = "SMDTV_60";
 	
-	public static void main(String []args){
-		 /*打开数据源*/
-		openDataSource();
-		
-		/*创建数据源*/
-//		createDataSource();
-		
-		/*创建面数据集*/
-		createDataSet();
-		
-	}
-	/*创建面数据集*/
-	public static void createDataSet(){
-		/*初始化半径*/
-		double radius = 22.22;
-		/*设置中心点坐标*/
-		Point2D  point2D =new Point2D(116, 39);
-		
-		GeoCircle geoCircle =new GeoCircle();
-		geoCircle.setRadius(radius);
-		geoCircle.setCenter(point2D);
-		
-		/*将圆几何对象转换为面几何对象。*/
-		geoCircle.convertToRegion(50);
-		
+	public static void main(String[]args){
+		createRegionDataSet();
 	}
 	
-	/*创建数据源*/
-	public static void createDataSource(){
+	/*创建圆形 的面数据集*/
+	public static void createRegionDataSet(){
 		Workspace workspace = new Workspace();
-		
-		DatasourceConnectionInfo info = new DatasourceConnectionInfo();
-		info.setEngineType(EngineType.ORACLEPLUS);
-		info.setServer(iobjectJavaServer);
-		info.setDatabase(iobjectJavaDatabase);
-		info.setUser(iobjectJavaUser);
-		info.setPassword(iobjectJavaPassword);
-		
-		Datasource ds = workspace.getDatasources().create(info);
-		//创建成功,输出数据源相关信息
-		if (ds != null) {
-			System.out.println("数据源创建成功!");
-		} else {
-			System.out.println("数据源创建失败!");
-		}
-		//关闭所有打开的数据源
-		workspace.getDatasources().closeAll();
-	}
-	
-	/*打开数据源*/
-	public static void openDataSource(){
-		Workspace workspace = new Workspace();
-		
 		// 定义数据源连接信息，假设以下所有数据源设置都存在
 		/*String  iobjectJavaServer = "10.10.68.248:1521/orcl";
 		String  iobjectJavaDatabase = "riskcontrol";
@@ -93,14 +53,42 @@ public class OperateDataSource {
       datasourceconnection.setAlias("ORACLE");
       // 打开数据源
       Datasource datasource = workspace.getDatasources().open(datasourceconnection);
+      
       // 获取的点数据集
-      DatasetVector datasetVector = (DatasetVector)datasource.getDatasets().get("RISKMAP_ADDRESS");
+//      DatasetVector datasetVector = (DatasetVector)datasource.getDatasets().get("RISKMAP_ADDRESS");
+      
       if (datasource == null) {
           System.out.println("打开数据源失败");
       } else {
          System.out.println("数据源打开成功！");
       }
+      
+      // 创建数据集
+      Datasets datasets = datasource.getDatasets();
+      DatasetVectorInfo datasetVectorInfo = new DatasetVectorInfo();
+       //面数据集类型
+      datasetVectorInfo.setType(DatasetType.REGION);
+	  datasetVectorInfo.setName("circleName");
+	  
+	  DatasetVector datasetVector = datasets.create(datasetVectorInfo);
+	  
+	  Recordset recordset = datasetVector.getRecordset(false, CursorType.DYNAMIC);
+	  
+	  	/*初始化半径*/
+		double radius = 22.22;
+		/*设置中心点坐标*/
+		Point2D  point2D =new Point2D(116, 39);
 		
+		GeoCircle geoCircle =new GeoCircle();
+		geoCircle.setRadius(radius);
+		geoCircle.setCenter(point2D);
+		
+		/*将圆几何对象转换为面几何对象。*/
+		GeoRegion geoRegion = geoCircle.convertToRegion(50);
+	  
+	    recordset.addNew(geoRegion);
+		
+	    
 		if(datasetVector!=null){
 			datasetVector.close();
 		}
@@ -118,4 +106,6 @@ public class OperateDataSource {
 		}
 		
 	}
+	
+
 }
