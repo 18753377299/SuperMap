1225f36c6a878f4984750fc02b8518426e72cc57
1733856225@qq.com <1733856225@qq.com>
2019-06-10 00:01:27 +0800

    练习

---
+++ src/javaPractice/base3/IntegerTest.java
@@ -0,0 +1,11 @@
+package javaPractice.base3;
+
+public class IntegerTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int max =Integer.MAX_VALUE;
+		System.out.println(max);
+	}
+
+}
