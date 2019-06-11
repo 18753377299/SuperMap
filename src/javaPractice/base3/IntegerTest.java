f3efcc3aff5b7c9c881df33cc15a0e2ee1a06b55
1733856225@qq.com <1733856225@qq.com>
2019-06-11 15:27:40 +0800

    git忽略2

--- src/javaPractice/base3/IntegerTest.java
+++ src/javaPractice/base3/IntegerTest.java
@@ -4,9 +4,8 @@ public class IntegerTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int max =Integer.MAX_VALUE;
-		
-		System.out.println(max+"");
+		int max =Integer.MAX_VALUE;		
+		System.out.println(max);
 	}
 
 }
