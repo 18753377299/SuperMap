fab5d84cb586b8098d9dd0761066857e97f4e3c8
1733856225@qq.com <1733856225@qq.com>
2019-06-14 16:47:15 +0800

    文件删除

--- src/javaPractice/base3/IntegerTest.java
+++ src/javaPractice/base3/IntegerTest.java
@@ -4,9 +4,9 @@ public class IntegerTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int max =Integer.MAX_VALUE;	
-		
+		int max =Integer.MAX_VALUE;			
 		System.out.println(max);
+		
 	}
 
 }
